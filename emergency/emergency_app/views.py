ffa7a7300c796029071970ca63f414cbe108a75e
Kremlin-dev <yawamp27@gmail.com>
2025-02-02 22:16:11 +0000

    added db connection in mongo

--- emergency/emergency_app/views.py
+++ emergency/emergency_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import employee_collection
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h1> App is running</h1>")
+
+def add_employee(request):
+    records ={
+        "name": "John",
+        "age": 12
+    }
+    employee_collection.insert_one(records)
+    return HttpResponse("New Person added")
+
+def get_all_employees(request):
+    employee = employee_collection.find()
+    return HttpResponse(employee)
