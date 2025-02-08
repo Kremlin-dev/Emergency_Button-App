a067c28c8643378fc61e35a8a3e6439f82e794f4
Kremlin-dev <yawamp27@gmail.com>
2025-02-08 00:51:43 +0000

    refresh token url added

--- emergency/emergency_app/urls.py
+++ emergency/emergency_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 
     path('register/', views.register, name = 'register'),
     path('login/', views.login, name='login'),
+    path('refresh-token/', views.refresh_token, name='refresh_token'),
+
 
 ]
