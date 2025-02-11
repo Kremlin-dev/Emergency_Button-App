6003c898c809c447c4bf54b35907197910f68f65
estherlardze <estherlardze8@gmail.com>
2025-02-11 15:52:49 +0000

    added client side

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <Button variant="outline">Button</Button>
+
+    </div>
+  )
+}
+
+export default App
